ffcb888c1abfc003939947640d9cc7473da9b9c3
Sergey Georgiev <sgrgv_art@mail.ru>
2025-03-06 16:59:04 +0300

    feat: добавил роуты

---
+++ src/app/api/Ext/GetSubscrsExt/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const targetUrl = "https://testlkamur.dvec.ru/Ext/GetSubscrsExt";
+
+  try {
+    const response = await fetch(targetUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...(req.headers.get("authorization") && {
+          Authorization: req.headers.get("authorization")!,
+        }),
+      },
+      body: await req.text(),
+    });
+
+    const data = await response.json();
+    return NextResponse.json(data, { status: response.status });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Ошибка при запросе к API" },
+      { status: 500 }
+    );
+  }
+}
