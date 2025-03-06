c7a3cd42576b74b2a3db0f85b14e7e0dbe1a356d
Sergey Georgiev <sgrgv_art@mail.ru>
2025-03-06 14:50:57 +0300

    feat: создан слайс account-slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,14 +3,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./slices/auth-slice";
-// import accountsReducer from "./slices/account-slice";
+import accountsReducer from "./slices/account-slice";
 import paymentsReducer from "./slices/payments-slice";
 import periodReducer from "./slices/period-slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    // accounts: accountsReducer,
+    accounts: accountsReducer,
     payments: paymentsReducer,
     period: periodReducer,
   },
